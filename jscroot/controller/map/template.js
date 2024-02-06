b446fe830fb93747e2a50df943590e66d9241ab5
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2024-02-06 09:23:13 +0700

    updates

--- jscroot/controller/map/template.js
+++ jscroot/controller/map/template.js
@@ -1,13 +1,13 @@
-export let xxx = "https://asia-southeast2-project3-403614.cloudfunctions.net/gischap4";
+export let xxx = "https://us-central1-exemplary-oven-401904.cloudfunctions.net/function-1";
 export let aaa = "https://asia-southeast2-exemplary-oven-401904.cloudfunctions.net/tambahpoint";
 export let bbb = "https://asia-southeast2-exemplary-oven-401904.cloudfunctions.net/tambahpolyline";
 export let ccc = "https://asia-southeast2-exemplary-oven-401904.cloudfunctions.net/tambahpolygon";
-export let qqq = "https://asia-southeast2-project3-403614.cloudfunctions.net/geointersect";
-export let www = "https://asia-southeast2-project3-403614.cloudfunctions.net/geowithin";
-export let eee = "https://asia-southeast2-project3-403614.cloudfunctions.net/near";
-export let rrr = "https://asia-southeast2-project3-403614.cloudfunctions.net/nearSphere";
-export let ttt = "https://asia-southeast2-project3-403614.cloudfunctions.net/geobox";
-export let yyy = "https://asia-southeast2-project3-403614.cloudfunctions.net/geocenter";
+export let qqq = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostGeoIntersects";
+export let www = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostGeoWithin";
+export let eee = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostNear";
+export let rrr = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostNearSphere";
+export let ttt = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostBox";
+export let yyy = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostCenter";
 export let uuu = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostCenterSphere";
 
 export let clickpopup = `
